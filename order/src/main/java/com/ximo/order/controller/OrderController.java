df215e14789f27a859d596ad05f224c31a980767
Xikl <1392936858@qq.com>
2018-10-07 16:46:41 +0800

    重构代码位置

--- order/src/main/java/com/ximo/order/controller/OrderController.java
+++ order/src/main/java/com/ximo/order/controller/OrderController.java
@@ -1,19 +1,14 @@
 package com.ximo.order.controller;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.ximo.order.domain.OrderDetail;
 import com.ximo.order.dto.OrderDTO;
 import com.ximo.order.enums.ResultEnums;
 import com.ximo.order.exception.OrderException;
 import com.ximo.order.form.OrderForm;
 import com.ximo.order.service.OrderService;
 import com.ximo.order.util.ConstraintUtil;
-import com.ximo.order.util.ConvertUtil;
 import com.ximo.order.vo.ResultVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,11 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.joining;
 
 /**
  * @author Ximo
@@ -52,7 +43,7 @@ public class OrderController {
         ConstraintUtil.orElseThrow(errors, log, logger -> logger.error("【创建订单】参数不正确， orderForm={}", orderForm));
 
         //转换为orderDTO对象
-        OrderDTO orderDTO = getOrderDTO(orderForm);
+        OrderDTO orderDTO = OrderDTO.getOrderDTO(orderForm);
         if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())) {
             log.error("【创建订单】购物车不能为空");
             throw new OrderException(ResultEnums.CART_EMPTY);
@@ -64,18 +55,7 @@ public class OrderController {
         return ResultVO.success(map);
     }
 
-    private OrderDTO getOrderDTO(OrderForm orderForm) {
-        try {
-            OrderDTO orderDTO = ConvertUtil.convert(orderForm, OrderDTO.class);
-            List<OrderDetail> orderDetailList =
-                    new Gson().fromJson(orderForm.getItems(), new TypeToken<List<OrderDetail>>() {}.getType());
-            orderDTO.setOrderDetailList(orderDetailList);
-            return orderDTO;
-        } catch (Exception e) {
-            log.error("【对象转换】json格式转换错误， String={}", orderForm.getItems());
-            throw new OrderException(ResultEnums.PARAM_ERROR);
-        }
-    }
+
 
 
 }
--- order/src/main/java/com/ximo/order/dto/OrderDTO.java
+++ order/src/main/java/com/ximo/order/dto/OrderDTO.java
@@ -1,6 +1,12 @@
 package com.ximo.order.dto;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.ximo.order.domain.OrderDetail;
+import com.ximo.order.enums.ResultEnums;
+import com.ximo.order.exception.OrderException;
+import com.ximo.order.form.OrderForm;
+import com.ximo.order.util.ConvertUtil;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -40,5 +46,16 @@ public class OrderDTO {
     /** 订单详情 */
     private List<OrderDetail> orderDetailList;
 
+    public static OrderDTO getOrderDTO(OrderForm orderForm) {
+        try {
+            OrderDTO orderDTO = ConvertUtil.convert(orderForm, OrderDTO.class);
+            List<OrderDetail> orderDetailList =
+                    new Gson().fromJson(orderForm.getItems(), new TypeToken<List<OrderDetail>>() {}.getType());
+            orderDTO.setOrderDetailList(orderDetailList);
+            return orderDTO;
+        } catch (Exception e) {
+            throw new OrderException(ResultEnums.PARAM_ERROR);
+        }
+    }
 
 }
